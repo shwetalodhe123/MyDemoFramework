f83e74cd629db18b960bb85343f707c0ad20f52a
Shweta Lodhe <shwetalodhe@gmail.com>
2020-12-03 19:49:38 +0530

    finished our framework

---
+++ MyDemoFramework/MyDemoFramework/MyDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyDemoFramework.h
+//  MyDemoFramework
+//
+//  Created by Shweta Suryakant Lodhe on 03/12/20.
+//  Copyright © 2020 theiOSDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDemoFramework.
+FOUNDATION_EXPORT double MyDemoFrameworkVersionNumber;
+
+//! Project version string for MyDemoFramework.
+FOUNDATION_EXPORT const unsigned char MyDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDemoFramework/PublicHeader.h>
+
+
---
+++ MyDemoFramework/MyDemoFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  MyDemoFramework
+//
+//  Created by Shweta Suryakant Lodhe on 03/12/20.
+//  Copyright © 2020 theiOSDev. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){}
+    
+    public static func DoSomething() -> String {
+        return "Successfully import dynamic framework"
+    }
+}
