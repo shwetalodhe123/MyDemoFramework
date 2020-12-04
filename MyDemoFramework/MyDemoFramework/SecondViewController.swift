b35ea4f8fab4a6ef394e6c56e7d799103c9cc242
Shweta Lodhe <shwetalodhe@gmail.com>
2020-12-04 15:10:16 +0530

    added bundle code

---
+++ MyDemoFramework/MyDemoFramework/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  MyDemoFramework
+//
+//  Created by Shweta Suryakant Lodhe on 04/12/20.
+//  Copyright © 2020 theiOSDev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
